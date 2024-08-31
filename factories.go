5b2ce3c66f9e06f52b057b2ed30bbb6eceab78f3
Sam <131374083+samsmi7h@users.noreply.github.com>
2024-08-31 14:54:54 +0100

    Refmt date

--- factories.go
+++ factories.go
@@ -20,9 +20,11 @@ func tickerFactory(lifespan time.Duration) makeNewTicker {
 
 type makeNewOutput func() io.WriteCloser
 
+const dateFmt = "2006-01-02T15:04:05"
+
 func fileFactory(dir string) makeNewOutput {
 	return func() io.WriteCloser {
-		fileName := fmt.Sprintf("%s_%s.log", uuid.NewString(), time.Now().Format(time.DateTime))
+		fileName := fmt.Sprintf("%s_%s.log", uuid.NewString(), time.Now().Format(dateFmt))
 		p := path.Join(dir, fileName)
 		f, err := os.Create(p)
 		must(err)
